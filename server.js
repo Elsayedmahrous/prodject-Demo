bfb1fc1d0edc8a79a8f0dfd6a7f6b855945b8a0a
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-08 12:01:44 +0200

    connect DB and first router in the project

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+const mountRouters = require('./Routes/index');
 
 const app = express();
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    console.log('Hello');
-});
+// DB
+dotenv.config({ path: 'config.env' });
+
+mongoose.connect(process.env.DB_URL).then((conn) => {
+    console.log(`Database Connected: ${conn.connection.host}`);
+})
 
 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+    console.log(`mode: ${process.env.NODE_ENV}`)
+  }
+
+// mountRouters
+mountRouters(app);
 const PORT = process.env.PORT || 3000
-const server = app.listen(PORT, () => {
+
+app.listen(PORT, () => {
     console.log(`App running ${PORT}`);
 })
