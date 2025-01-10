e2a80aa2a342c3c2826c73b72b6af4f463d1cbce
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-10 21:00:10 +0200

    add signup validation

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const { signup } = require('../services/authService');
+const { signupValidator} = require('../utils/Validators/authValidators')
 
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post('/signup', signupValidator ,signup);
 
 module.exports = router;
