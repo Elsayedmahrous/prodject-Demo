ab687ae0baee9a3aaefa67ab111a3d144fe41249
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-11 15:59:30 +0200

    add some routes for example service and validator and logged

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,11 +1,36 @@
 const express = require('express');
 
-const { createUser, getOne,getAll,updateUser, deleteOne } = require('../services/userService');
-const { createUserValidator } = require('../utils/Validators/userValidators')
+const {
+    createUser,
+    getOne,
+    getAll,
+    updateUser,
+    deleteOne,
+    changeUserPassword,
+    getLoggedUserData,
+    updateLoggedUserData,
+    updateLoggedUserPassword,
+    deleteLoggedUserData
+}
+    = require('../services/userService');
+const {
+    createUserValidator,
+    getUserValidator,
+    updateUserValidator,
+    deleteUserValidator,
+    changeUserPasswordValidator,
+    updateLoggedUserValidator
+}
+    = require('../utils/Validators/userValidators')
 
 const router = express.Router();
-
-router.route('/').post(createUserValidator, createUser).get(getAll);
-router.route('/:id').get(getOne).put(updateUser).delete(deleteOne);
+router.get('/:getMe', getLoggedUserData,getAll);
+router.put('/:updateMe', updateLoggedUserValidator, updateLoggedUserData);
+router.put('/changeMyPassword', updateLoggedUserPassword);
+router.delete('/deleteMe', deleteLoggedUserData)
+//* Admin
+router.put('/changePassword/:id',changeUserPasswordValidator,changeUserPassword)
+router.route('/').post(createUserValidator, createUser).get(getUserValidator,getAll);
+router.route('/:id').get(getOne).put(updateUserValidator,updateUser).delete(deleteUserValidator,deleteOne);
 
 module.exports = router;
