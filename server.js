9b36642da5b5a41a609c7f827675f1e7276d4c33
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-11 16:06:43 +0200

    update in mode part

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const mountRouters = require('./Routes/index');
+const apiError = require('./utils/ApiError')
 
 const app = express();
 app.use(express.json())
@@ -15,15 +16,20 @@ mongoose.connect(process.env.DB_URL).then((conn) => {
 })
 
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_DEV === 'Development') {
     app.use(morgan('dev'));
-    console.log(`mode: ${process.env.NODE_ENV}`)
+    console.log(`mode: ${process.env.NODE_DEV}`)
   }
 
 // mountRouters
 mountRouters(app);
+
+app.all('*', (req, res, next) => {
+    next(new apiError(`Can not find this route :${req.originalUrl}`, 400))
+});
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`App running ${PORT}`);
-})
+});
