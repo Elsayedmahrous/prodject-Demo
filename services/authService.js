8e9cd939447a04e4f8e1462a45da6d28d865aa5f
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-14 20:38:54 +0200

    add function to make roles users before request and add function to make forget password when users forget your password and password reset code before return wirte password and hash reset code in db

--- services/authService.js
+++ services/authService.js
@@ -1,8 +1,10 @@
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const asyncHandler = require('express-async-handler');
 const User = require('../Models/userSchema');
 const createToken = require('../utils/createToken');
+const sendEmail = require('../utils/sendEmail');
 const ApiError = require('../utils/ApiError');
 
 /**
@@ -18,6 +20,7 @@ exports.signup = asyncHandler(async (req, res, next) => {
         password: req.body.password,
         email: req.body.email,
         userName: req.body.userName,
+        role: req.body.role,
     });
     //2) Generate token
     const token = await createToken(user._id);
@@ -25,7 +28,6 @@ exports.signup = asyncHandler(async (req, res, next) => {
     res.status(201).json({ data: user, token });
 });
 /**
- * 
  * @desc     login
  * @route    Get/api/v1/auth/login
  * @access   Pubic
@@ -61,7 +63,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     }
     //* 4) check of user change his password after token create
     if (currentUser.passwordChangeAt, decoded.iat) {
-        const passwordChangeAtTimestamp = parseInt(currentUser.passwordChangeAt.getTime() / 1000, 10);
+        const passwordChangeAtTimestamp = parseInt(currentUser.passwordChangeAt.getTime()/ 1000, 10);
         if (passwordChangeAtTimestamp > decoded.iat) {
             return next(new ApiError('User recently changed his password. please login again...', 401));
         }
@@ -80,4 +82,38 @@ exports.allowedTo = (...roles) => asyncHandler(async (req, res, next) => {
         return next(new ApiError('You not allowed to access this route', 403));
     }
     next()
+});
+/**
+ * @desc     forgetPassword
+ * @route    Post/api/v1/auth/forgetPassword
+ * @access   Pubic
+ */
+exports.forgetPassword = asyncHandler(async (req, res, next) => {
+    //* 1)- Get user by email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return next(new ApiError(`there is no user with that email ${req.body.email}`, 404));
+    }
+    //* 2)- if user exist, Generate reset random 6 digits and save it in db
+    const resetCode = Math.floor(100000 + Math.random() * 900000).toString();
+    const hashedResetCode = await crypto.createHash('sha256').update(resetCode).digest('hex');
+    user.passwordResetCode = hashedResetCode;
+    user.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+    user.passwordResetVerified = false;
+    await user.save();
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'your password reset code (valid 10 M)',
+            message: `Hi ${user.name},\n I hope this email finds you well`,
+        });
+    } catch (err) {
+        user.passwordResetCode = undefined;
+        user.passwordResetExpires = undefined;
+        user.passwordResetVerified = undefined;
+        await user.save();
+        return next(new ApiError(`There is an error in sending email, ${err}`, 500));
+    }
+
+    res.status(200).json({ status: "Success", message: 'Reset code send to email' });
 })
