5b4d3cbb45f7762231d2b25f137d51913f86fd98
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-14 20:24:40 +0200

    add login validator and forgetPassword

--- Routes/authRoute.js
+++ Routes/authRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
-const { signup, login } = require('../services/authService');
-const { signupValidator} = require('../utils/Validators/authValidators')
+const { signup, login , forgetPassword } = require('../services/authService');
+const { signupValidator, loginValidator} = require('../utils/Validators/authValidators')
 
 const router = express.Router();
 
 router.post('/signup', signupValidator, signup);
-router.post('/login', login);
+router.post('/login',loginValidator, login);
+router.post('/forgetPassword', forgetPassword);
 
 
 module.exports = router;
