3427db9a83ddd1701cbf17970d53e2cb32aa5a39
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-15 20:56:25 +0200

    add new Route call Room

--- Routes/index.js
+++ Routes/index.js
@@ -1,9 +1,11 @@
 const authRouter = require('./authRoute');
 const userRouter = require('./userRoute')
+const roomRouter = require('./roomRoute')
 
 const mountRouters = (app) => {
     app.use('/api/v1/auth', authRouter);
     app.use('/api/v1/users', userRouter);
+    app.use('/api/v1/Room', roomRouter);
 }
 
 
