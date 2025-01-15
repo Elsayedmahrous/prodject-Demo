cf853632dfa6ecad88c0c733344934b470008bd1
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-15 20:57:29 +0200

    first commit about this is file call room route

---
+++ Routes/roomRoute.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const {
+    getRooms,
+    getRoom,
+    createRoom,
+    updateRoom,
+    deleteRoom
+} = require('../services/roomService');
+
+const {
+    createRoomValidator,
+    getRoomValidator,
+    updateRoomValidator,
+    deleteRoomValidator
+
+ } = require('../utils/Validators/roomValidator');
+
+const authService = require('../services/authService');
+
+const router = express.Router();
+//* Manager
+router.use(authService.protect, authService.allowedTo('Manager'));
+router.route('/').post(createRoomValidator, createRoom).get(getRooms);
+router.route('/id').get(getRoomValidator,getRoom).put(updateRoomValidator,updateRoom).delete(deleteRoomValidator,deleteRoom);
+
+module.exports = router;
