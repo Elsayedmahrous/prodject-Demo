a636e72970da49a26958063a352ba54ce1523b60
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-15 21:04:56 +0200

    first commit this is file

---
+++ services/roomService.js
@@ -0,0 +1,33 @@
+const factory = require('../services/handlersFactory');
+const Room = require('../Models/roomSchema');
+
+/**
+ * @desc  Create Room
+ * @route  Post /api/v1/Room
+ * @access Manager
+ */
+exports.createRoom = factory.createOne(Room);
+/**
+ * @desc  Get Rooms
+ * @route  Get /api/v1/Room
+ * @access Manager
+ */
+exports.getRooms = factory.getOne(Room);
+/**
+ * @desc  Get specific room
+ * @route  Get /api/v1/Room
+ * @access Manager
+ */
+exports.getRoom = factory.getOne(Room);
+/**
+ * @desc  update specific room
+ * @route  Put /api/v1/Room
+ * @access Manager
+ */
+exports.updateRoom = factory.updateUser(Room);
+/**
+ * @desc  delete specific room
+ * @route  Delete /api/v1/Room
+ * @access Manager
+ */
+exports.deleteRoom = factory.deleteOne(Room);
