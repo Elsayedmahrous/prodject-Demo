93c07bae5041c35193a0f9d3a77adc95aa33608e
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-16 16:23:18 +0200

    add function call addUserToRoom do add user in the room and add function call getUsersToRoom do get all users in the room and add function call deleteUserFromRoom do remove userId in the room

--- services/roomService.js
+++ services/roomService.js
@@ -1,5 +1,8 @@
+const asyncHandler = require('express-async-handler');
+const ApiError = require('../utils/ApiError');
 const factory = require('../services/handlersFactory');
 const Room = require('../Models/roomSchema');
+const User = require('../Models/userSchema')
 
 /**
  * @desc  Create Room
@@ -31,3 +34,58 @@ exports.updateRoom = factory.updateUser(Room);
  * @access Manager
  */
 exports.deleteRoom = factory.deleteOne(Room);
+/**
+ * @desc  add specific userId in roomId  
+ * @route  Post /api/v1/Room/id/add-member
+ * @access Manager
+ */
+exports.addUserToRoom = asyncHandler(async (req, res, next) => {
+    const room = await Room.findById(req.params.id);
+    if (!room) {
+        return next(new ApiError('Room not found', 404));
+    }
+    const { userId } = req.body;
+    const user = await User.findById(userId);
+    if (!user) {
+        return next(new ApiError('user not found in this is room', 404));
+    }
+    if (room.users.includes(userId)) {
+        return next(new ApiError('User already exists'), 401);
+    };
+    room.users.push(userId);
+    await room.save();
+
+    res.status(200).json({ message: 'User added successfully', room });
+});
+/**
+ * @desc  delete specific userId in roomId  
+ * @route  Delete /api/v1/Room/id/delete-member
+ * @access Manager
+ */
+exports.deleteUserFromRoom = asyncHandler(async (req, res, next) => {
+    const room = await Room.findById(req.params.id);
+    if (!room) {
+        return next(new ApiError('Room not found', 404));
+    }
+    const { userId } = req.body;
+    if (!room.users.includes(userId)) {
+        return next(new ApiError('User already exists'), 401);
+    };
+    room.users.pull(userId);
+    await room.save();
+
+    res.status(200).json({ message: 'User remove successfully' });
+});
+/**
+ * @desc  Get users in roomId  
+ * @route  Get /api/v1/Room/id/get-member
+ * @access Manager
+ */
+exports.getUsersToRoom = asyncHandler(async (req, res, next) => {
+    const room = await Room.findById(req.params.id);
+    if (!room) {
+        return next(new ApiError('Room not found by Id', 404));
+    };
+    const Members = room.users;
+    res.status(200).json({ Members });
+})
