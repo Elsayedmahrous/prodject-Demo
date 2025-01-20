e24cd2915d2a9cacea0e37de4ae834e1ad71f262
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-20 14:26:05 +0200

    remove the comment

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,9 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-// const createToken =  jwt.sign({ userId: user._Id }, process.env.JWT_SECRET_KEY, {
-//     expiresIn: process.env.JWT_EXPIRE_TIME
-// });
-
 const createToken = (payload) => 
     jwt.sign({ userId: payload }, process.env.JWT_SECRET_KEY, {
         expiresIn:process.env.JWT_EXPIRE_TIME,
