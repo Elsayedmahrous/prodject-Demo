825a48dd5631469263e97f2a0e3c05d2a04c4a4f
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-21 20:25:00 +0200

    add new route it is task route

--- Routes/index.js
+++ Routes/index.js
@@ -1,11 +1,13 @@
 const authRouter = require('./authRoute');
-const userRouter = require('./userRoute')
-const roomRouter = require('./roomRoute')
+const userRouter = require('./userRoute');
+const roomRouter = require('./roomRoute');
+const taskRouter = require('./taskRoute')
 
 const mountRouters = (app) => {
     app.use('/api/v1/auth', authRouter);
     app.use('/api/v1/users', userRouter);
     app.use('/api/v1/Room', roomRouter);
+    app.use('/api/v1/task', taskRouter);
 }
 
 
