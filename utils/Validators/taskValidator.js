400bbdfd23889f53953ce3ab74bc52bf9eb24f40
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-22 19:51:23 +0200

    add validator before update and  delete and get

--- utils/Validators/taskValidator.js
+++ utils/Validators/taskValidator.js
@@ -17,3 +17,39 @@ exports.createTaskValidator = [
         .withMessage('completed is required'),
     validationMiddleware
 ];
+/**
+ * @desc    get specific by Id to do list to Room
+ * @route   Get/api/v1/task
+ * @access  Admin /Manager
+ */
+exports.getTaskFormRoomValidator = [
+    check('id').isMongoId().withMessage('Invalid ID'),
+    validationMiddleware
+];
+/**
+ * @desc    delete specific by Id to do list to Room
+ * @route   delete/api/v1/task
+ * @access  Admin /Manager
+ */
+exports.deleteTaskFormRoomValidator = [
+    check('id').isMongoId().withMessage('Invalid ID'),
+    validationMiddleware
+];
+/**
+ * @desc    update specific by Id to do list to Room
+ * @route   put/api/v1/task
+ * @access  Admin /Manager
+ */
+exports.updateTaskInRoomValidator = [
+    check('id').isMongoId().withMessage('Invalid ID'),
+    check('name')
+        .optional()
+        .notEmpty()
+        .withMessage('task is required')
+        .isLength({ min: 4 }).withMessage('Task name must be least 4 characters long'),
+    check('completed')
+        .optional()
+        .notEmpty()
+        .withMessage('completed is required'),
+    validationMiddleware
+];
