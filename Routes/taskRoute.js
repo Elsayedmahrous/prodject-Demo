bc71ce51f7eef7b8fcd863823c4141db5f38de25
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-26 16:12:23 +0200

    delete route call delete task from room

--- Routes/taskRoute.js
+++ Routes/taskRoute.js
@@ -4,14 +4,12 @@ const {
     createTaskToRoom,
     getTaskFromRoom,
     updateTaskInRoom,
-    deleteTaskFromRoom
 } = require('../services/taskService');
 
 const {
     createTaskValidator,
     getTaskFormRoomValidator,
     updateTaskInRoomValidator,
-    deleteTaskFormRoomValidator
 } = require('../utils/Validators/taskValidator')
 
 const authService = require('../services/authService');
@@ -19,9 +17,8 @@ const authService = require('../services/authService');
 const router = express.Router();
 
 router.post('/:roomId/tasks', authService.protect, createTaskValidator, createTaskToRoom);
-router.get('/:id', authService.protect,getTaskFormRoomValidator, getTaskFromRoom);
+router.get('/:roomId/tasks', authService.protect,getTaskFormRoomValidator, getTaskFromRoom);
 router.put('/:roomId/task/:taskId', authService.protect,updateTaskInRoomValidator, updateTaskInRoom);
-router.delete('/:id', authService.protect,deleteTaskFormRoomValidator, deleteTaskFromRoom);
 
 
 module.exports = router;
