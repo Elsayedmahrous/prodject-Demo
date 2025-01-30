4243a424cbf8497d12769bcc09239b613c988518
Elsayed <elsayed.r.mahrous@gmail.com>
2025-01-30 20:08:51 +0200

    add new route in the file call chat route

--- Routes/index.js
+++ Routes/index.js
@@ -1,13 +1,15 @@
 const authRouter = require('./authRoute');
 const userRouter = require('./userRoute');
 const roomRouter = require('./roomRoute');
-const taskRouter = require('./taskRoute')
+const taskRouter = require('./taskRoute');
+const chatRouter = require('./chatRoute');
 
 const mountRouters = (app) => {
     app.use('/api/v1/auth', authRouter);
     app.use('/api/v1/users', userRouter);
     app.use('/api/v1/Room', roomRouter);
     app.use('/api/v1/task', taskRouter);
+    app.use('/api/v1/chat', chatRouter);
 }
 
 
